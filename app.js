6246c259316fb85e996464f681d9ad8761f01722
MCCTUA <mcctua@gmail.com>
2023-01-22 11:23:06 +0700

    install CORS and arrange line app.js

--- app.js
+++ app.js
@@ -1,22 +1,25 @@
 const express = require("express");
+const cors = require("cors");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
-
-require("dotenv").config();
-
+const dotenv = require("dotenv");
 const indexRouter = require("./routes/index");
 const lineRouter = require("./routes/line");
 
+dotenv.config();
 const app = express();
+app.use(cors()); // allow all origin
 
 app.use(logger("dev"));
+app.use(express.static(path.join(__dirname, "public")));
+
+// Line แนะนำ ให้วาง Route ก่อน express.json
+app.use("/", indexRouter);
+app.use("/line", lineRouter);
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
-
-app.use("/", indexRouter);
-app.use("/line", lineRouter);
 
 module.exports = app;
