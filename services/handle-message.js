dc4baf70288bfcbeddf864de71242b8461afbfb9
MCCTUA <mcctua@gmail.com>
2023-01-22 23:45:17 +0700

    formating

--- services/handle-message.js
+++ services/handle-message.js
@@ -4,22 +4,16 @@ const { sendLocation } = require("./send-location");
 const { sendText } = require("./send-text");
 
 exports.handleMessage = (event) => {
-  // Basic 1
-  //   if (event.type !== "message" || event.message.type !== "text") {
-  //     // ignore non-text-message event
-  //     return Promise.resolve(null);
-  //   }
-
-  //   // create a echoing text message
-  //   const echo = { type: "text", text: event.message.text };
   let msg;
 
   switch (event.message.text.toLowerCase().trim()) {
     case "image":
       msg = sendImage();
       break;
+
     case "location":
       msg = sendLocation();
+
       break;
     default:
       msg = sendText(event);
